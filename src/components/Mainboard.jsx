39b8b2418f9e4f95df012d7d0b14c351dc2983c8
hydrogenbond68 <hydrogenbond68@gmail.com>
2025-01-03 07:50:43 +0300

    new update

--- src/components/Mainboard.jsx
+++ src/components/Mainboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Pin from "./Pin";
 
+
 function Mainboard(props) {
   let { pins } = props;
 
@@ -25,26 +26,15 @@ const Wrapper = styled.div`
   width: 100%;
   height: 100%;
   margin-top: 100px;
-  justify-content: center;  
+  justify-content: center;
+  
 `;
 
 const Container = styled.div`
   margin: 0 auto;
-  width: 80%; // Default for larger screens
+  height: 100%;
+  width: 100%;
   background-color: white;
-  display: flex;
-  flex-wrap: wrap; // Allows items to wrap to the next line
-  justify-content: center;
-  align-items: center;
-  padding: 20px; // To avoid content touching the edge on smaller screens
 
-  @media (max-width: 768px) {
-    width: 100%; // Full width on mobile
-    padding: 10px; // Less padding for smaller screens
-  }
-
-  @media (min-width: 769px) {
-    // Desktop-specific adjustments here if needed
-    padding: 20px 0; // More vertical padding for desktops
-  }
+  
 `;
